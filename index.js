53811b723473d7cf444ad99e6d6b42770837b0f0
Cansu Aysagdic <cansu.aysagdic@ogr.iu.edu.tr>
2023-03-24 14:45:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution in this file!
+
+let arrayOfDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+
+//First test:
+const returnFirstTwoDrivers = function(arrayOfDrivers) {
+ return arrayOfDrivers.slice(0,2);
+}
+
+//Second test:
+const returnLastTwoDrivers = function(arrayOfDrivers) {
+return arrayOfDrivers.slice(-2);
+}
+
+//Third test:
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+//createFareMultiplier();
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+}
+const quadruples = createFareMultiplier(4);
+
+//fareDoubler();
+
+function fareDoubler(fare) {
+    return fare * 2;
+}
+
+//fareTripler();
+
+function fareTripler(fare) {
+    return fare * 3;
+}
+
+//selectedDifferDrivers();
+
+function selectDifferentDrivers(drivers, fn) {
+    return fn(drivers);
+  }
